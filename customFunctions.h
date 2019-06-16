a661fda8d14dbddeb928aa6149c7ec5ac78f2b8c
PiotrWierzbicki <32744445+PiotrWierzbicki@users.noreply.github.com>
2019-06-16 15:15:06 +0200

    Delete customFunctions.h

--- customFunctions.h
+++
@@ -1,9 +0,0 @@
-#ifndef CUSTOMFUNCTION_H
-#define CUSTOMFUNCTION_H
-
-int partition(double data[], int p, int r);
-void quicksort(double data[], int p, int r);
-float percentile95th(double data[], int N);
-double *getBestNParents(double data[], int N);
-
-#endif // !CUSTOMFUNCTION_H
